74e7467fb2a850bf856df7c7e5249cf5a71a4f54
Mazharul Islam <mazharul430@gmail.com>
2022-11-17 13:26:42 -0800

    added cards and button component

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Chare from '../../../assets/images/chair.png';
 import bannerImg from '../../../assets/images/bg.png';
-const Banner = () => {
+import icon1 from '../../../assets/icons/clock.svg';
+import icon2 from '../../../assets/icons/marker.svg';
+import icon3 from '../../../assets/icons/phone.svg';
+import ButtonComponent from '../../../Components/ButtonComponent/ButtonComponent';
 
+
+const Banner = () => {
     return (
-        <section className='py-10 px-5 lg:px-0 ' style ={{
+        <section className='py-10 px-5 lg:px-0 ' style={{
             background: `url(${bannerImg})`
         }}>
-            
+
             <div className="flex items-center my-10 flex-col lg:flex-row-reverse mx-auto gap-10">
                 <div className='lg:w-1/2'>
                     <img src={Chare} alt='' className="rounded-lg shadow-2xl" />
@@ -16,10 +21,38 @@ const Banner = () => {
                     <div className=''>
                         <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                         <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
-                        <button className="btn btn-primary">Get Started</button>
+                       <ButtonComponent>Get Started</ButtonComponent>
                     </div>
                 </div>
             </div>
+
+            <div className='grid lg:grid-cols-3 gap-5 my-20 mt-36'>
+
+                <div className=" p-7 bg-gradient-to-r from-secondary to-primary card card-side text-white shadow-xl">
+                    <figure><img src={icon1}alt="Movie" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Opening Hours</h2>
+                        <p>Lorem Ipsum is simply dummy text of the pri</p>
+                    </div>
+                </div>
+                <div className=" p-7 bg-accent text-white card card-side shadow-xl">
+                    <figure><img src={icon2} alt="Movie" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Visit Our Locations</h2>
+                        <p>Brooklyn, NY 10036, United States</p>
+                    </div>
+                </div>
+                <div className=" p-7 bg-gradient-to-r from-secondary to-primary text-white card card-side bg-base-100 shadow-xl">
+                    <figure><img src={icon3} alt="Movie" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Contact Us Now</h2>
+                        <p>+8801766-430106</p>
+                    </div>
+                </div>
+            </div>
+
+
+
         </section>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+
+  daisyui: {
+    themes: [
+      {
+        doctorstheme: {
+          'primary': '#0FCFEC',
+          'secondary': '#19D3AE',
+          "accent": "#3A4256",
+          "neutral": "#291334",
+          "base-100": "#FAF7F5",
+        }
+      }
+    ]
+  },
+
+
   theme: {
     extend: {},
   },
